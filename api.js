539c3c5366e35bf7e48771f30932238fa3f8bc75
Mile <milevuletic@national.shitposting.agency>
2020-07-04 00:52:52 +0200

    Documented API

--- api.js
+++ api.js
@@ -15,12 +15,21 @@ import socketio from 'socket.io-client';
 const apiPrefix  = 'https://api.bitwave.tv/api/';
 const chatServer = 'https://chat.bitwave.tv';
 
-
+/**
+ * Gets a new troll token from the API server.
+ * @return JWT token as string
+ */
 const getTrollToken = async () => {
     const data = await $http.get( apiPrefix + 'troll-token' );
     return data;
 };
 
+/**
+ * Uses @p credentials to get a token from the server.
+ * Note: currently ignores credentials and gets a troll token.
+ *
+ * @return JWT token as string
+ */
 const initToken = async credentials => {
     //TODO: use credentials
     const data = await getTrollToken();
@@ -30,14 +39,27 @@ const initToken = async credentials => {
 /* ========================================= */
 
 let socket = null;
+
+/**
+ * This function is called when connecting to the server
+ */
 const socketConnect = () => {
     socket.emit( 'new user', userProfile );
     $log.info( `Connected to chat! (${userProfile.page})` );
 };
+
+/**
+ * This function is called when the server issues a reconnect.
+ * It force hydrates chat to catch up.
+ */
 const socketReconnect = async hydrate => {
     $log.info( "Socket issued 'reconnect'. Forcing hydration..." );
     await hydrate();
 };
+
+/**
+ * This function is called when there's a socket error.
+ */
 const socketError = message => {
     $log.error( `Socket error: ${message}` );
     // TODO: handle error
@@ -51,15 +73,30 @@ let userProfile = {
 
 export default {
 
-    global: true,
+    global: true, /**< Global chat mode flag */
 
-    get socket() { return socket; },
+    get socket() { return socket; }, /**< socketio object */
     set socket( s ) { socket = s; },
 
+    /**
+     * Callback function that receives messages (in bulk)
+     * @param ms Message object array
+     */
     rcvMessageBulk: ms => { for( const m of ms ) console.log( m ); },
+
+    /**
+     * Callback function that receives paid chat alert objects
+     * @param message Alert object
+     */
     alert( message ) { $log.warn( `Recieved alert: ${message}` ); },
 
-    channelViewers: [],
+    channelViewers: [], /**< Array of channel viewers.  */
+
+    /**
+     * Gets an array of usernames from the server and puts it in channelViewers
+     * It is called automatically at request from the server, but can be called manually
+     * @see channelViewers
+     */
     async updateUsernames() {
         const data = await $http.get( 'https://api.bitwave.tv/v1/chat/channels' );
         if( data && data.success ) {
@@ -67,7 +104,11 @@ export default {
         }
     },
 
-    // TODO: This is called when the connection gets wobbly
+    /**
+     * Requests messages from the server (called hydration)
+     * It is called automatically when reconnecting.
+     * @see socketError()
+     */
     hydrate() {
         const data = $http.get( 'https://chat.bitwave.tv/v1/messages' + this.room ? this.room : '' );
         if( !data.length ) return $log.warn( 'Hydration data was empty' );
@@ -77,10 +118,11 @@ export default {
         return undefined;
     },
 
-    // Inits data and starts connection to server
-    // 'room' is a string for the channel you wish to connect to
-    // 'credentials', if falsy, gets a new troll token
-    //   if a string, it's taken as the JWT chat token
+    /**
+     * Inits data and starts connection to server
+     * @param room is a string for the channel you wish to connect to
+     * @param credentials User credentialsif falsy, gets a new troll token. If a string, it's taken as the JWT chat token
+     */
     async init( room, credentials ) {
         if( credentials && typeof credentials == 'string' ) {
             userProfile.token = credentials;
@@ -112,15 +154,16 @@ export default {
         // this.socket.on( 'pollstate', data => this.updatePoll( data ) );
     },
 
-    get room()  { return userProfile.page; },
+    get room()  { return userProfile.page; }, /**< Current room */
     set room(r) {
         userProfile.page = r;
         $log.info( `Changed to room ${r}` );
     },
 
-    // Sends message with current config (this.userProfile)
-    // 'msg' can be an object: { message, channel, global, showBadge }
-    //   or just a string (in which case channel/global use current values)
+    /**
+     * Sends message with current config (this.userProfile)
+     * @param msg Message to be sent. Can be an object: { message, channel, global, showBadge }, or just a string (in which case channel/global use current values)
+     */
     sendMessage( msg ) {
         switch( typeof msg ) {
         case 'object':
